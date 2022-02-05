4e726a0ce9a9cd759ab8bbb148682af57b3f147a
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-02-05 17:41:52 -0600

    inquirer prompts incorrect format

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const Employee = require('./lib/Employee')
 const promptUser = () => {
 
     Employee.prototype.getName();
+    Employee.prototype.getId();
+    Employee.prototype.getEmail();
+    Employee.prototype.getRole();
 }
 
 promptUser();
