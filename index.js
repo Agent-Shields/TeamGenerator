ede67c09650eedda67d4e2ec4860ca2e07f78092
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-02-05 17:38:33 -0600

    trying to work with classes and inheritence

--- index.js
+++ index.js
@@ -1,60 +1,9 @@
 const inquirer = require('inquirer');
+const Employee = require('./lib/Employee')
 
 const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'managerName',
-            message: "What is the team manager's name",
-            validate: nameInput =>  {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log("Please enter the team manager's name!");
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'managerID',
-            message: "What is the team manager's employee ID?",
-            validate: employeeIDInput =>  {
-                if (employeeIDInput) {
-                    return true;
-                } else {
-                    console.log("Please enter the team manager's employee ID!");
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'managerEmail',
-            message: "What is the team manager's email address?",
-            validate: managerEmailInput =>  {
-                if (managerEmailInput) {
-                    return true;
-                } else {
-                    console.log("Please enter the team manager's email address!");
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'managerOffice',
-            message: "What is the team manager's office number?",
-            validate: managerOfficeInput =>  {
-                if (managerOfficeInput) {
-                    return true;
-                } else {
-                    console.log("Please enter the team manager's office number!");
-                    return false;
-                }
-            }
-        }
-    ]);
+
+    Employee.prototype.getName();
 }
 
 promptUser();
