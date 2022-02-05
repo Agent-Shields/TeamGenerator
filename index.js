f29858cb371199f4fa1a8389047867208aac2425
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-02-05 11:58:40 -0600

    basic class template setup for employee, basic test structure for each class

--- index.js
+++ index.js
@@ -14,8 +14,47 @@ const promptUser = () => {
                     return false;
                 }
             }
+        },
+        {
+            type: 'input',
+            name: 'managerID',
+            message: "What is the team manager's employee ID?",
+            validate: employeeIDInput =>  {
+                if (employeeIDInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the team manager's employee ID!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is the team manager's email address?",
+            validate: managerEmailInput =>  {
+                if (managerEmailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the team manager's email address!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerOffice',
+            message: "What is the team manager's office number?",
+            validate: managerOfficeInput =>  {
+                if (managerOfficeInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the team manager's office number!");
+                    return false;
+                }
+            }
         }
-    ])
+    ]);
 }
 
 promptUser();
