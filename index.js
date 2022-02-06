447def85d699977fa2f1ae81ddec29005145d3ba
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-02-05 19:06:37 -0600

    updated class structure, restructured index.js to hold inquirer functionality

--- index.js
+++ index.js
@@ -1,12 +1,83 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee')
+const Employee = require('./lib/Employee');
+const fs = require('fs');
 
-const promptUser = () => {
 
-    Employee.prototype.getName();
-    Employee.prototype.getId();
-    Employee.prototype.getEmail();
-    Employee.prototype.getRole();
+// list of questions to be prompted regarding employee data
+const questions = 
+[
+    {
+        type: 'input',
+        name: 'employeeName',
+        message: "What is the employee's name?",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("You need to enter the employee's name!")
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'employeeID',
+        message: "What is the employee's ID?",
+        validate: employeeIDInput => {
+            if (employeeIDInput) {
+                return true;
+            } else {
+                console.log("You need to enter the employee's ID!")
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'employeeEmail',
+        message: "What is the employee's email address?",
+        validate: employeeEmailInput => {
+            if (employeeEmailInput) {
+                return true;
+            } else {
+                console.log("You need to enter the employee's email address!")
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'employeeRole',
+        message: "What is the employee's role?",
+        choices: ["Manager", "Engineer", "Intern"],
+        validate: employeeRoleInput => {
+            if (employeeRoleInput) {
+                return true;
+            } else {
+                console.log("You need to enter the employee's role!")
+                return false;
+            }
+        }
+    }
+];
+    
+// empty object which within we will store the inquirer response data
+var questionAnswers = {};
+
+// ask questions to the user via inquirer
+function promptUser() {
+    return inquirer
+    .prompt(questions)
+    .then(questionsData => {
+        questionAnswers = questionsData;
+        console.log(questionAnswers);
+    })
+};
+
+// TODO: Create function to write HTML file to dist
+const writeFile = () => {
+    
 }
 
+// starts asking questions upon node index.js invokation 
 promptUser();
